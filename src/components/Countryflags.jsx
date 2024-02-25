1ca8509dc5e43c733fed04d94ff887cbd62e56c1
Harikalyan99 <harikalyan99tronics@gmail.com>
2024-02-25 22:06:28 +0530

    container error fixed

--- src/components/Countryflags.jsx
+++ src/components/Countryflags.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import styles from './Countryflags.module.css';
 
 const Countryflags = ({countryFlags}) => {
   return (
     <div style={{display: "flex", flexWrap: "wrap", justifyContent: "center"}}>
         {countryFlags.map((country, ind) => (
-            <div key={ind} style={{height: "200px", width: "200px", border: "3px solid #efefef", borderRadius: "5px", margin: "10px", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", padding: "10px"}}>
+            <div key={ind} className={styles.countryCard} >
             <img src={country.flags.svg} alt={country.flags.alt} height={100}/>
             <h4>{country.name.common}</h4>
             </div>
