37d82a91d7344a8d3c62feb513da20773949e8d0
Harikalyan99 <harikalyan99tronics@gmail.com>
2024-02-25 23:56:07 +0530

    search error fixed 2

--- src/components/Countryflags.jsx
+++ src/components/Countryflags.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import styles from './Countryflags.module.css';
 
 const Countryflags = ({countryFlags}) => {
   return (
     <div style={{display: "flex", flexWrap: "wrap", justifyContent: "center"}}>
         {countryFlags.map((country, ind) => (
-            <div key={ind} className={styles.countryCard} >
+            <div key={ind} className="countryCard" >
             <img src={country.flags.svg} alt={country.flags.alt} height={100}/>
             <h2>{country.name.common}</h2>
             </div>
